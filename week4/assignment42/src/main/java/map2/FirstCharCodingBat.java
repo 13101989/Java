1ae45ad63e6f1725f89acfc99ad347f11fbd2be6
Valentin Damache <valentindamache@Valentins-MBP.lan>
2024-07-15 20:49:54 +0300

    work on map 1 coding bat problems

---
+++ week4/assignment42/src/main/java/map2/FirstCharCodingBat.java
@@ -0,0 +1,28 @@
+package main.java.map2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstCharCodingBat {
+    public static void main(String[] args) {
+        FirstCharCodingBat object = new FirstCharCodingBat();
+
+        Map<String, String> params = new HashMap<>();
+
+
+
+    }
+
+    /**
+     * Given an array of non-empty strings, return a Map<String, String> with a key for every different
+     * first character seen, with the value of all the strings starting with that character appended together
+     * in the order they appear in the array.
+     * firstChar(["salt", "tea", "soda", "toast"]) → {"s": "saltsoda", "t": "teatoast"}
+     * firstChar(["aa", "bb", "cc", "aAA", "cCC", "d"]) → {"a": "aaaAA", "b": "bb", "c": "cccCC", "d": "d"}
+     * firstChar([]) → {}
+     */
+
+    public Map<String, String> firstChar(String[] strings) {
+
+    }
+}
