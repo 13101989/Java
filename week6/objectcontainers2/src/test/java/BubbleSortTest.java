b452b3a51a6e1d056e66fd78d6e3d02fadf96c5f
13101989 <35628219+13101989@users.noreply.github.com>
2024-07-28 19:13:10 +0300

    work on objectcontainer2 assignment

---
+++ week6/objectcontainers2/src/test/java/BubbleSortTest.java
@@ -0,0 +1,16 @@
+package test.java;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BubbleSortTest {
+
+    @Test
+    void sort() {
+    }
+
+    @Test
+    void reverse() {
+    }
+}
