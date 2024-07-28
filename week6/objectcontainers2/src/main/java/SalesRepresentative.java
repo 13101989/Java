5cd4fe37b581559644cdb8f9ce2f13970bb3ff7f
13101989 <35628219+13101989@users.noreply.github.com>
2024-07-28 20:31:32 +0300

    work on objectcontainer2 assignment

--- week6/objectcontainers2/src/main/java/SalesRepresentative.java
+++ week6/objectcontainers2/src/main/java/SalesRepresentative.java
@@ -1,10 +1,21 @@
 package main.java;
 
+/**
+ * Represents a sales representative with a number of sales, quota per sale, and revenue generated.
+ * The revenue generated is calculated as the product of the number of sales and the quota per sale.
+ */
 public class SalesRepresentative {
     private final int numberOfSales;
     private final int quotaPerSale;
     private final int revenueGenerated;
 
+    /**
+     * Constructs a SalesRepresentative object with the specified number of sales and quota per sale.
+     * The revenue generated is calculated as the product of the number of sales and the quota per sale.
+     *
+     * @param numberOfSales the number of sales made by the sales representative
+     * @param quotaPerSale the quota per sale for the sales representative
+     */
     public SalesRepresentative(int numberOfSales, int quotaPerSale) {
         this.numberOfSales = numberOfSales;
         this.quotaPerSale = quotaPerSale;
