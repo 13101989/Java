e04863397608872910ceec50862a40bce65ccd1c
13101989 <35628219+13101989@users.noreply.github.com>
2024-07-28 14:49:51 +0300

    work on testing assignment

--- week6/testing/src/test/java/CalculatorTest.java
+++ week6/testing/src/test/java/CalculatorTest.java
@@ -1,21 +1,31 @@
 package test.java;
 
 import main.java.Calculator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class CalculatorTest {
 
+    private Calculator calculator;
+
+    @BeforeEach
+    void set_up(){
+        calculator = new Calculator();
+    }
+
     @Test
+    @DisplayName("add: 1 m + 2 mm = 1.002 m")
     void add() {
-        Calculator calculator = new Calculator();
         assertEquals("1 m + 2 mm = 1.002 m", calculator.add(1, "m", 2, "mm", "m"));
     }
 
     @Test
+    @DisplayName("subtract: 1 cm - 2 mm = 0.8 cm")
     void subtract() {
-        Calculator calculator = new Calculator();
         assertEquals("1 cm - 2 mm = 0.8 cm", calculator.subtract(1, "cm", 2, "mm", "cm"));
     }
 }
