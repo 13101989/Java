34209ce977bf31d4222a9d524799ec296996c6e0
13101989 <35628219+13101989@users.noreply.github.com>
2024-08-04 16:27:20 +0300

    work on quiz assignment

--- week7/quizz/src/main/java/Coordinate.java
+++ week7/quizz/src/main/java/Coordinate.java
@@ -1,21 +1,10 @@
 package main.java;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class Coordinate {
     private final double x, y;
-
-    public Coordinate(double x, double y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public double getX() {
-        return x;
-    }
-
-    public double getY() {
-        return y;
-    }
 }
