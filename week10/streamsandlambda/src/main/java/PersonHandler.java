b0cf2421fc4b7dbc7000a99f457b364cf3ef97cd
13101989 <35628219+13101989@users.noreply.github.com>
2024-08-25 18:28:02 +0300

    work on streams and lambda assignment

--- week10/streamsandlambda/src/main/java/PersonHandler.java
+++ week10/streamsandlambda/src/main/java/PersonHandler.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import lombok.Getter;
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -13,6 +15,7 @@ import java.util.stream.Stream;
 
 import static main.java.Person.PERSON_COMPARATOR;
 
+@Getter
 public class PersonHandler {
     private List<Person> persons = new ArrayList<>();
 
@@ -40,11 +43,61 @@ public class PersonHandler {
         writePersonsToFile(fileToWrite, personsBornInSameMonth);
     }
 
+    public List<String> findAndWriteToFilePersonsBornInSpecificMonth(List<String> csvToRead, int month, String fileToWrite) throws IOException {
+        readAndParsePersonsFromCsvFile(csvToRead);
+
+        List<String> personsBornInSameMonth =
+                persons
+                        .stream()
+                        .filter(person -> person.getDateOfBirth().getMonth().getValue() == month)
+                        .sorted(PERSON_COMPARATOR)
+                        .map(Person::toString)
+                        .collect(Collectors.toList());
+
+        String firstLine = Stream.of(
+                personsBornInSameMonth.isEmpty()
+                        ? "No person from list provided is born in the month of " + Month.of(month)
+                        : "Persons that are born in " + Month.of(month) + " ordered alphabetically:"
+        ).findFirst().orElse("");
+
+        personsBornInSameMonth.addFirst(firstLine);
+
+        personsBornInSameMonth.forEach(System.out::println);
+
+        return personsBornInSameMonth;
+    }
+
     private void readAndParsePersonsFromCsvFile(String csvToRead) throws IOException {
         String csvDelimiter = ",";
         Path file = Paths.get(csvToRead);
 
         persons = Files.readAllLines(file)
+                .stream()
+                .skip(1)
+                .map(line -> {
+                    String[] personData = line.split(csvDelimiter);
+
+                    String personFirstName = personData[1];
+                    String personLastName = personData[2];
+
+                    int personBirthYear = Integer.parseInt(personData[3]);
+                    int personBirthMonth = Integer.parseInt(personData[4]);
+                    int personBirthDay = Integer.parseInt(personData[5]);
+                    LocalDate personBirthDate = LocalDate.of(personBirthYear, personBirthMonth, personBirthDay);
+
+                    return new Person(personFirstName, personLastName, personBirthDate);
+                })
+                .collect(Collectors.toList());
+
+        if (persons.isEmpty()) {
+            throw new IOException("CSV file doesn't contain any person!");
+        }
+    }
+
+    private void readAndParsePersonsFromCsvFile(List<String> csvToRead) throws IOException {
+        String csvDelimiter = ",";
+
+        persons = csvToRead
                 .stream()
                 .skip(1)
                 .map(line -> {
