18ebde75d8f67cbdb939a0a51720957d0310113b
13101989 <35628219+13101989@users.noreply.github.com>
2024-08-25 17:06:52 +0300

    work on streams and lambda assignment

--- week10/streamsandlambda/src/main/java/PersonHandler.java
+++ week10/streamsandlambda/src/main/java/PersonHandler.java
@@ -8,20 +8,28 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class PersonHandler {
-    private final List<Person> persons = new ArrayList<>();
+    private List<Person> persons = new ArrayList<>();
 
     public void findAndWriteToFilePersonsBornInSpecificMonth(String csvToRead, int month, String fileToWrite) throws IOException {
         readAndParsePersonsFromCsvFile(csvToRead);
 
-        List<String> personsBornInSameMonth = new ArrayList<>();
-        for (Person person : persons) {
-            if (person.getDateOfBirth().getMonth().getValue() == month) {
-                personsBornInSameMonth.add(person.toString());
-            }
-        }
-        personsBornInSameMonth.sort(null);
+//        List<String> personsBornInSameMonth = new ArrayList<>();
+//        for (Person person : persons) {
+//            if (person.getDateOfBirth().getMonth().getValue() == month) {
+//                personsBornInSameMonth.add(person.toString());
+//            }
+//        }
+//        personsBornInSameMonth.sort(null);
+
+        List<String> personsBornInSameMonth =
+                persons.stream()
+                        .filter(person -> person.getDateOfBirth().getMonth().getValue() == month)
+                        .map(Person::toString)
+                        .sorted()
+                        .collect(Collectors.toList());
 
         if (!personsBornInSameMonth.isEmpty()) {
             System.out.println("Persons that are born in " + Month.of(month) + " ordered alphabetically:");
@@ -42,25 +50,49 @@ public class PersonHandler {
     private void readAndParsePersonsFromCsvFile(String csvToRead) throws IOException {
         String csvDelimiter = ",";
         Path file = Paths.get(csvToRead);
-        List<String> lines = Files.readAllLines(file);
 
-        if (lines.size() < 2) {
+//        List<String> lines = Files.readAllLines(file);
+//        lines.remove(0);
+//
+//        if (lines.isEmpty()) {
+//            throw new IOException("CSV file doesn't contain any person!");
+//        }
+//
+//        for (String line : lines) {
+//            String[] personData = line.split(csvDelimiter);
+//
+//            String personFirstName = personData[1];
+//            String personLastName = personData[2];
+//
+//            int personBirthYear = Integer.parseInt(personData[3]);
+//            int personBirthMonth = Integer.parseInt(personData[4]);
+//            int personBirthDay = Integer.parseInt(personData[5]);
+//            LocalDate personBirthDate = LocalDate.of(personBirthYear, personBirthMonth, personBirthDay);
+//
+//            Person person = new Person(personFirstName, personLastName, personBirthDate);
+//            persons.add(person);
+//        }
+
+        persons = Files.readAllLines(file)
+                .stream()
+                .skip(1)
+                .map(line -> {
+                    String[] personData = line.split(csvDelimiter);
+
+                    String personFirstName = personData[1];
+                    String personLastName = personData[2];
+
+                    int personBirthYear = Integer.parseInt(personData[3]);
+                    int personBirthMonth = Integer.parseInt(personData[4]);
+                    int personBirthDay = Integer.parseInt(personData[5]);
+                    LocalDate personBirthDate = LocalDate.of(personBirthYear, personBirthMonth, personBirthDay);
+
+                    return new Person(personFirstName, personLastName, personBirthDate);
+                })
+                .collect(Collectors.toList());
+
+        if (persons.isEmpty()) {
             throw new IOException("CSV file doesn't contain any person!");
-        }
-
-        for (int i = 1; i < lines.size(); i++) {
-            String[] personData = lines.get(i).split(csvDelimiter);
-
-            String personFirstName = personData[1];
-            String personLastName = personData[2];
-
-            int personBirthYear = Integer.parseInt(personData[3]);
-            int personBirthMonth = Integer.parseInt(personData[4]);
-            int personBirthDay = Integer.parseInt(personData[5]);
-            LocalDate personBirthDate = LocalDate.of(personBirthYear, personBirthMonth, personBirthDay);
-
-            Person person = new Person(personFirstName, personLastName, personBirthDate);
-            persons.add(person);
         }
     }
 
