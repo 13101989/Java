63e493e3c96135a23d24f023eaecbfc51e75cb2c
13101989 <35628219+13101989@users.noreply.github.com>
2024-09-01 18:46:09 +0300

    work on concurrency assignment

--- week11/concurrency/src/main/java/FestivalAttendeeThread.java
+++ week11/concurrency/src/main/java/FestivalAttendeeThread.java
@@ -19,7 +19,7 @@ public class FestivalAttendeeThread implements Runnable {
     @Override
     public void run() {
         try {
-            Thread.sleep(500);
+            Thread.sleep(1000);
         } catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
