d3a30cc94235c1627bf0f64f6bae9357906dd2ed
13101989 <35628219+13101989@users.noreply.github.com>
2024-09-01 16:15:23 +0300

    work on concurrency assignment

--- week11/concurrency/src/main/java/FestivalStatisticsThread.java
+++ week11/concurrency/src/main/java/FestivalStatisticsThread.java
@@ -1,32 +1,39 @@
 package main.java;
 
 
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
 public class FestivalStatisticsThread implements Runnable {
     private final FestivalGate festivalGate;
 
     private int totalAttendees;
-    private int attendeesWithFullAccess;
-    private int attendeesWithFullVIPAccess;
-    private int attendeesWithFreePass;
-    private int attendeesWithOneDayAccess;
-    private int attendeesWithOneDayVIPAccess;
+    private long attendeesWithFullAccess;
+    private long attendeesWithFullVIPAccess;
+    private long attendeesWithFreePass;
+    private long attendeesWithOneDayAccess;
+    private long attendeesWithOneDayVIPAccess;
 
     public FestivalStatisticsThread(FestivalGate festivalGate) {
         this.festivalGate = festivalGate;
     }
 
     public void collectGateStats() {
-        totalAttendees = this.festivalGate.getAttendees().size();
+        List<FestivalAttendeeThread> attendees = this.festivalGate.getAttendees();
+        totalAttendees = attendees.size();
 
-        for (FestivalAttendeeThread attendee : this.festivalGate.getAttendees()) {
-            switch (attendee.getTicketType()) {
-                case Full -> attendeesWithFullAccess += 1;
-                case FullVIP -> attendeesWithFullVIPAccess += 1;
-                case FreePass -> attendeesWithFreePass += 1;
-                case OneDay -> attendeesWithOneDayAccess += 1;
-                case OneDayVIP -> attendeesWithOneDayVIPAccess += 1;
-            }
-        }
+        Map<TicketType, Long> ticketCountMapper = attendees
+                .stream()
+                .collect(Collectors.groupingBy(
+                        FestivalAttendeeThread::getTicketType,
+                        Collectors.counting()));
+
+        attendeesWithFullAccess = ticketCountMapper.getOrDefault(TicketType.Full, 0L);
+        attendeesWithFullVIPAccess = ticketCountMapper.getOrDefault(TicketType.FullVIP, 0L);
+        attendeesWithFreePass = ticketCountMapper.getOrDefault(TicketType.FreePass, 0L);
+        attendeesWithOneDayAccess = ticketCountMapper.getOrDefault(TicketType.OneDay, 0L);
+        attendeesWithOneDayVIPAccess = ticketCountMapper.getOrDefault(TicketType.OneDayVIP, 0L);
     }
 
     @Override
